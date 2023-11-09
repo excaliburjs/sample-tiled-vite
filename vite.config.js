b2a6e62768903fea80567ea65c047eb828372c90
Erik Onarheim <erik.onarheim@gmail.com>
2023-11-09 17:39:00 -0600

    Theoretically fix vite builds

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite';
 
+const tiledPlugin = () => {
+    return {
+        name: 'tiled-tileset-plugin',
+        resolveId: {
+            order: 'pre',
+            handler(sourceId, importer, options) {
+                if (!sourceId.endsWith(".tsx")) return;
+                return { id: 'tileset:' + sourceId, external: 'relative' }
+            }
+        }
+    };
+}
+
 export default defineConfig({
-    publicDir: 'res',
-    assetsInclude: [ '**/*.tmx'],
-})
+    plugins: [tiledPlugin()], // hint vite that tiled tilesets should be treated as external
+    build: {
+        assetsInlineLimit: 0, // excalibur cannot handle inlined xml in prod mode
+        sourcemap: true
+    }
+});
